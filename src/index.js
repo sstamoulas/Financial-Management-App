d44c873b3c4969eab3a39cc742a24b2c2dc1968a
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-04-23 15:49:13 +0300

    Added craco for building with create-react-app and purge-css, added purge-css to craco.config.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import "purecss";
-
 
 import { store, persistor } from './redux/store';
 
