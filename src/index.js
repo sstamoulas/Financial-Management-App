34c828130f59080c48a00773dd2db7e180a9038c
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-04-23 13:33:30 +0300

    Removed isomorphic-style-loader and added purifyCss so unused css will not affect the render time, remove source maps from build

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import "purecss";
+
 
 import { store, persistor } from './redux/store';
 
